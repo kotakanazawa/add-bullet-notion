d6c7e75e9455d139ebab7d88c3a2700ec388e25f
Kota Kanazawa <kota0818@gmail.com>
2025-10-01 20:00:24 +0900

    Notion専用にする

--- content.js
+++ content.js
@@ -1,10 +1,5 @@
 // Command + Shift + 8 でハイフンを入力
 function insertBulletPoint(activeElement) {
-  // githubの場合は処理をやめる. デフォルトでショートカットキーが使えるため
-  if (window.location.hostname === "github.com") {
-    return
-  }
-
   // contentEditableの場合(Notion用)
   if (activeElement.isContentEditable) {
     document.execCommand("insertText", false, "- ")
@@ -16,23 +11,6 @@ function insertTodo(activeElement) {
   // contentEditableの場合(Notion用)
   if (activeElement.isContentEditable) {
     document.execCommand("insertText", false, "[] ")
-  }
-  // input/textareaの場合
-  else {
-    const start = activeElement.selectionStart
-    const end = activeElement.selectionEnd
-    const value = activeElement.value
-    const todoText = "- [ ] "
-
-    // カーソル位置に`- [ ] `を挿入. Github用
-    activeElement.value =
-      value.substring(0, start) + todoText + value.substring(end)
-
-    // カーソル位置を更新
-    activeElement.selectionStart = activeElement.selectionEnd = start + todoText.length
-
-    // inputイベントを発火（Reactなどのフレームワーク対応）
-    activeElement.dispatchEvent(new Event("input", { bubbles: true }))
   }
 }
 
